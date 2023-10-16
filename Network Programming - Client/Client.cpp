1cd79c3539e37698b419b9d7fa48ab0eee0d2731
EHARD <ehardesty987@gmail.com>
2023-10-16 14:43:47 -0500

    working client to server with TCP - CLIENT PORTION

--- Network Programming - Client/Client.cpp
+++ Network Programming - Client/Client.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-int main() {  // Changed the function name from "client" to "main"
+int main() {
     WSADATA wsaData;
     int wsaerr;
     WORD wVersionRequested = MAKEWORD(2, 2);
@@ -19,11 +19,11 @@ int main() {  // Changed the function name from "client" to "main"
     cout << "The Status: " << wsaData.szSystemStatus << endl;
 
     // Create a socket
-    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);  // Changed "serverSocket" to "clientSocket"
-    if (clientSocket == INVALID_SOCKET) {  // Changed "serverSocket" to "clientSocket"
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSocket == INVALID_SOCKET) {
         cout << "Error at socket(): " << WSAGetLastError() << endl;
         WSACleanup();
-        return 0;
+        return 1;  // Changed the return value to 1 on error
     }
     else {
         cout << "socket() is good" << endl;
@@ -32,11 +32,11 @@ int main() {  // Changed the function name from "client" to "main"
     sockaddr_in clientService;
     clientService.sin_family = AF_INET;
     InetPton(AF_INET, L"127.0.0.1", &clientService.sin_addr.s_addr);
-    clientService.sin_port = htons(8080);  // Added a semicolon here
+    clientService.sin_port = htons(8080);
 
-    if (connect(clientSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) {  // Fixed the if statement
+    if (connect(clientSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) {
         cout << "Client: Connect() - Failed to connect." << endl;
-        closesocket(clientSocket);  // Close the socket on failure
+        closesocket(clientSocket);
         WSACleanup();
         return 1;  // Return 1 on failure
     }
@@ -44,7 +44,30 @@ int main() {  // Changed the function name from "client" to "main"
         cout << "Client: connect() is good." << endl;
         cout << "Client: Can start sending and receiving data..." << endl;
     }
-    system("pause");
+
+    char buffer[200];
+    printf("Enter your Message ");
+    cin.getline(buffer, 200);
+    int byteCount = send(clientSocket, buffer, 200, 0);
+    if (byteCount == SOCKET_ERROR) {
+        printf("Client send error %ld.\n", WSAGetLastError());  // Changed "Server" to "Client"
+        return -1;
+    }
+    else {
+        printf("Client: sent %d bytes \n", byteCount);  // Changed "byteCount" to "%d"
+    }
+
+    // Receiving
+
+    char receiveBuffer[200] = "";
+    byteCount = recv(clientSocket, receiveBuffer, 200, 0);  // Changed "acceptSocket" to "clientSocket"
+    if (byteCount < 0) {
+        printf("Client: error %ld.\n", WSAGetLastError());
+        return 0;
+    }
+    else {
+        printf("Received data: %s \n", receiveBuffer);
+    }
     WSACleanup(); // Cleanup Winsock when done
     return 0;
 }
