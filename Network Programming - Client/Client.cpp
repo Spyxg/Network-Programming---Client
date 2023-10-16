c373f50daec96ae62905f3fc60ea71bf6fcd1676
EHARD <ehardesty987@gmail.com>
2023-10-15 21:41:27 -0500

    Add project files.

---
+++ Network Programming - Client/Client.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+using namespace std;
+
+int main() {  // Changed the function name from "client" to "main"
+    WSADATA wsaData;
+    int wsaerr;
+    WORD wVersionRequested = MAKEWORD(2, 2);
+
+    wsaerr = WSAStartup(wVersionRequested, &wsaData);
+    if (wsaerr != 0) {
+        cout << "WSAStartup failed with error: " << wsaerr << endl;
+        return 1;
+    }
+
+    cout << "The Winsock DLL found!" << endl;
+    cout << "The Status: " << wsaData.szSystemStatus << endl;
+
+    // Create a socket
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);  // Changed "serverSocket" to "clientSocket"
+    if (clientSocket == INVALID_SOCKET) {  // Changed "serverSocket" to "clientSocket"
+        cout << "Error at socket(): " << WSAGetLastError() << endl;
+        WSACleanup();
+        return 0;
+    }
+    else {
+        cout << "socket() is good" << endl;
+    }
+
+    sockaddr_in clientService;
+    clientService.sin_family = AF_INET;
+    InetPton(AF_INET, L"127.0.0.1", &clientService.sin_addr.s_addr);
+    clientService.sin_port = htons(8080);  // Added a semicolon here
+
+    if (connect(clientSocket, (SOCKADDR*)&clientService, sizeof(clientService)) == SOCKET_ERROR) {  // Fixed the if statement
+        cout << "Client: Connect() - Failed to connect." << endl;
+        closesocket(clientSocket);  // Close the socket on failure
+        WSACleanup();
+        return 1;  // Return 1 on failure
+    }
+    else {
+        cout << "Client: connect() is good." << endl;
+        cout << "Client: Can start sending and receiving data..." << endl;
+    }
+    system("pause");
+    WSACleanup(); // Cleanup Winsock when done
+    return 0;
+}
